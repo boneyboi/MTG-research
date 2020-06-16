829015bd389e937a430dab201a406f19170955e3
Alkaroth <michaelrbowling@gmail.com>
2020-06-16 10:33:46 -0500

    Added a facade class

---
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/Facade.java
@@ -0,0 +1,28 @@
+package forge.game.research.decision;
+
+public class Facade {
+
+    /**
+     * Get Attackers (param: List of possible attackers, List of possible blockers)
+     * (return list of creatures)
+     *
+     * Get Blockers (param: List of attackers, List of possible blockers)
+     * (return as map)
+     *
+     * Get decision (param: List of options/SpellAbilities)
+     * (return Spell Ability)
+     *
+     * Mulligan (param: Hand, Mull #)
+     * (return List of cards to put back, or null if we want to mulligan)
+     *
+     * Forced Discard (param: Hand)
+     * (return card)
+     *
+     * Forced Sacrifice (param: List of cards that can be sacrificed)
+     * (return card)
+     *
+     * Damage on Double block (param: Attacker power, list of defenders)
+     * (return List of integers)
+     *
+    */
+}
