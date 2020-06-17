5cf72fe1b157c25a35fe4641bf0317262215cdd6
boneyboi <50offgame@gmail.com>
2020-06-17 11:45:26 -0400

    Added Card Template

---
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/CardTemplate.java
@@ -0,0 +1,13 @@
+package forge.game.research.decision;
+
+import forge.game.Game;
+import forge.game.card.Card;
+
+public abstract class CardTemplate extends Card {
+
+
+    public CardTemplate(int id0, Game game0) {
+        super(id0, game0);
+    }
+
+}
