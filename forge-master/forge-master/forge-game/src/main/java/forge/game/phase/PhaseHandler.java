abf8e31cbbf372f61ec6f28950536ec68834d841
Alkaroth <michaelrbowling@gmail.com>
2020-06-17 14:50:56 -0500

    Added a LifeBuff Template

--- forge-master/forge-master/forge-game/src/main/java/forge/game/phase/PhaseHandler.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/phase/PhaseHandler.java
@@ -531,7 +531,7 @@ public class PhaseHandler implements java.io.Serializable {
         System.out.println();
         CardTemplate test = new TemplateName("Notion Rain");
         CardTemplate test2 = new TemplateCMC(1);
-        CardTemplate test3 = new TemplateLifelink();
+        CardTemplate test3 = new TemplateLifeBuff();
         for (Card card: getPlayerTurn().getZone(ZoneType.Hand)) {
             if (!card.isLand()) {
                 System.out.println(card.getName());
