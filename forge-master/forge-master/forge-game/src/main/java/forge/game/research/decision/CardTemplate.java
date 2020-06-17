26f817ac9b2bda42e8e509143368d6c4f5356633
boneyboi <50offgame@gmail.com>
2020-06-17 11:48:41 -0400

    Added Card Template

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/CardTemplate.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/CardTemplate.java
@@ -6,8 +6,22 @@ import forge.game.card.Card;
 public abstract class CardTemplate extends Card {
 
 
+
+    /**
+     * A template for cards to be played in the strategy class
+     * @param id0
+     * @param game0
+     */
     public CardTemplate(int id0, Game game0) {
         super(id0, game0);
     }
 
+    /**
+     * Returns whether or not the card matches the template
+     * @param card
+     * @return
+     */
+    public boolean matches(Card card){
+        return false;
+    }
 }
--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/StrategyNode.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/StrategyNode.java
@@ -5,8 +5,8 @@ import forge.game.research.DoublyLinkedList;
 
 public class StrategyNode {
 
-    private DoublyLinkedList<Card> requirements;
-    private DoublyLinkedList<Card> cards;
+    private DoublyLinkedList<CardTemplate> requirements;
+    private DoublyLinkedList<CardTemplate> cards;
 
     public StrategyNode(StrategyNode node){
         this.requirements = node.requirements;
