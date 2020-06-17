0df6bb79fd27622c9900b9a343057c6b8997b558
Alkaroth <michaelrbowling@gmail.com>
2020-06-17 12:02:43 -0500

    Template Name idea

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/CardTemplate.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/CardTemplate.java
@@ -3,17 +3,14 @@ package forge.game.research.decision;
 import forge.game.Game;
 import forge.game.card.Card;
 
-public abstract class CardTemplate extends Card {
-
+public abstract class CardTemplate{
 
 
     /**
      * A template for cards to be played in the strategy class
-     * @param id0
-     * @param game0
      */
-    public CardTemplate(int id0, Game game0) {
-        super(id0, game0);
+    public CardTemplate() {
+
     }
 
     /**
