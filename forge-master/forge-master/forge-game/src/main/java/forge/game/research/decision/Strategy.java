e642a064412e5a0818190909bf4aef27b58c35e5
Alkaroth <michaelrbowling@gmail.com>
2020-06-17 10:27:28 -0500

    Created a name variable

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/Strategy.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/Strategy.java
@@ -9,17 +9,20 @@ public class Strategy{
     private DoublyLinkedList<StrategyNode> path;
     private DoublyLinkedList<Card> requirements;
     private DoublyLinkedList<Card> cards;
+    private String name;
 
      //TODO: Add variables to let this create a strategy off of a file import.
 
-    public Strategy(DoublyLinkedList<Card> requirements, DoublyLinkedList<Card> cards) {
+    public Strategy(DoublyLinkedList<Card> requirements, DoublyLinkedList<Card> cards, String name) {
         path = new DoublyLinkedList<StrategyNode>();
         this.requirements = requirements;
         this.cards = cards;
+        this.name = name;
         path.pushFront(new StrategyNode());
     }
 
     public void CheckRequirements(){
 
     }
+
 }
