787fb5772669e8f023088cfcdb76660f08b605db
boneyboi <50offgame@gmail.com>
2020-06-18 16:13:27 -0400

    added changes to Facade

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/ViablePlays.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/ViablePlays.java
@@ -11,12 +11,6 @@ public class ViablePlays {
     ArrayList<SpellAbility> plays = new ArrayList<SpellAbility>();
     int manaPossible = 0;
     Player controller = null;
-    int mountainNum = 0;
-    int swampNum = 0;
-    int islandNum = 0;
-    int forestNum = 0;
-    int plainsNum = 0;
-
 
     public ViablePlays(Player p) {
         controller = p;
@@ -28,41 +22,13 @@ public class ViablePlays {
         return plays;
     }
 
-
     public void getMana() {
+        //TODO: Account for sources that provide more than one mana?
         manaPossible = 0;
-        mountainNum = 0;
-        swampNum = 0;
-        islandNum = 0;
-        forestNum = 0;
-        plainsNum = 0;
-        for (Card c: controller.getZone(ZoneType.Battlefield)) {
-            for (SpellAbility sa : c.getManaAbilities()) {
-                String type = sa.getMapParams().get("Produced");
-                if (type.contains("R")) {
-                        mountainNum += 1;
-                        manaPossible += 1;
-                    }
-                if (type.contains("B")) {
-                        swampNum += 1;
-                    manaPossible += 1;
-                    }
-                if (type.contains("U")) {
-                        islandNum += 1;
-                    manaPossible += 1;
-                    }
-                if (type.contains("G")) {
-                        forestNum += 1;
-                    manaPossible += 1;
-                    }
-                if (type.contains("W")) {
-                        plainsNum += 1;
-                    manaPossible += 1;
-                    }
-
+        for (Card c: controller.getZone(ZoneType.Battlefield))
+            if (!c.getManaAbilities().isEmpty() && c.isUntapped()) {
+                manaPossible += 1;
             }
-            manaPossible += 1 - c.getManaAbilities().size();
-        }
         if (controller.getZone(ZoneType.Hand).contains(Card::isLand)
                 && controller.getLandsPlayedThisTurn() == 0) {
             manaPossible+=1;
@@ -70,11 +36,12 @@ public class ViablePlays {
     }
 
     public void addZoneOptions(ZoneType z) {
+        //ComputerUtilCost costComp = new ComputerUtilCost();
         for (Card c: controller.getZone(z)) {
             for (SpellAbility sa: c.getNonManaAbilities()) {
-                if (sa.getPayCosts().getTotalMana().getCMC() <= manaPossible
-                        && sa.canPlay()) {
+                if (sa.getPayCosts().getTotalMana().getCMC() <= manaPossible) {
                     plays.add(sa);
+                    c.getManaCost().getColorProfile();
                 }
             }
         }
