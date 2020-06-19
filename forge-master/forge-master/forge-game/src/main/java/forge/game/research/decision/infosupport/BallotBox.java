fb6a586a90143fc9848f3c200b34b2e4489eaa7f
Alkaroth <michaelrbowling@gmail.com>
2020-06-19 13:31:00 -0500

    Added option gatherer to ballotbox

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/infosupport/BallotBox.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/infosupport/BallotBox.java
@@ -9,12 +9,27 @@
 package forge.game.research.decision.infosupport;
 
 import forge.game.card.Card;
+import forge.game.player.Player;
 import forge.game.research.decision.strategy.Strategy;
 import forge.game.research.decision.strategy.StrategyNode;
+import forge.game.spellability.SpellAbility;
+
+import java.util.ArrayList;
 
 public class BallotBox {
+    public Player controller;
+    public ArrayList<SpellAbility> nonlands;
+    public ArrayList<Card> lands;
 
-    public BallotBox(){}
+    public BallotBox(Player p){
+        controller = p;
+    }
+
+    public void getOptions() {
+        ViablePlays vp = new ViablePlays(controller);
+        nonlands = vp.getNonlandPlays();
+        lands = vp.getLandPlays();
+    }
 
     /**
      * Return the card that is voted on
