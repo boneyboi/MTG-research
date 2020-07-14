b574afec9152e6b29ca1ea7035d234e2fc88f0d0
Alkaroth <michaelrbowling@gmail.com>
2020-07-14 13:23:52 -0500

    Set Framework

--- forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/combat/Blocking.java
+++ forge-master/forge-master/forge-game/src/main/java/forge/game/research/decision/combat/Blocking.java
@@ -1,4 +1,29 @@
 package forge.game.research.decision.combat;
 
+import forge.game.card.Card;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Blocking {
+
+    public void Blocking(){
+
+    }
+
+    public Map<Card, ArrayList<Card>> getBlocks(ArrayList<Card> attackers, ArrayList<Card> blockers) {
+        return getBlocksThird(getBlocksSecond(getBlocksFirst(attackers, blockers)));
+    }
+
+    public Map<Card, ArrayList<Card>> getBlocksFirst(ArrayList<Card> attackers, ArrayList<Card> blockers) {
+
+    }
+
+    public Map<Card, ArrayList<Card>> getBlocksSecond(Map<Card, ArrayList<Card>> list) {
+
+    }
+
+    public Map<Card, ArrayList<Card>> getBlocksThird(Map<Card, ArrayList<Card>> list) {
+
+    }
 }
--- forge-master/forge-master/forge-game/src/test/java/forge/game/research/deckstrategytestmicheal.java
+++ forge-master/forge-master/forge-game/src/test/java/forge/game/research/deckstrategytestmicheal.java
@@ -5,6 +5,8 @@ import forge.game.research.decision.strategy.StrategyNode;
 import forge.game.research.decision.strategy.template.TemplatePermanentCMC;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class deckstrategytestmicheal {
 
     @Test
@@ -12,6 +14,94 @@ public class deckstrategytestmicheal {
         DeckStrategy testing = new DeckStrategy("test strat");
         assert  (testing.getStrategies().isEmpty()) : "testing not created";
     }
+
+    public class Case {
+        int option = 0;
+        String name;
+        public Case(String call) {
+            name = call;
+        }
+
+        public Case(Case c) {
+            this.name = c.name;
+            this.option = c.option;
+        }
+
+        public int getOption() {
+            return option;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setOption(int amt) {
+            option = amt;
+        }
+    }
+
+    @Test
+    public void clock() {
+        ArrayList<String> attacks = new ArrayList<>();
+        attacks.add("a");
+        attacks.add("b");
+        attacks.add("c");
+        attacks.add("d");
+        attacks.add("e");
+        attacks.add("f");
+        attacks.add("g");
+        attacks.add("h");
+        attacks.add("i");
+        attacks.add("j");
+        attacks.add("end");
+
+        ArrayList<Case> blocks = new ArrayList<>();
+        blocks.add(new Case("1"));
+        blocks.add(new Case("2"));
+        blocks.add(new Case("3"));
+        blocks.add(new Case("4"));
+        blocks.add(new Case("5"));
+
+        int loops = 0;
+        boolean check = false;
+
+        Case selected;
+        boolean done;
+        while (blocks.get(blocks.size()-1).getOption() != (attacks.size() - 1)) {
+            if (blocks.get(0).getOption() == 7 &&
+                blocks.get(1).getOption() == 7 &&
+                blocks.get(2).getOption() == 7 &&
+                blocks.get(3).getOption() == 7 &&
+                blocks.get(4).getOption() == 7) {
+                check = true;
+            }
+            loops ++;
+            int get = 0;
+            done = false;
+
+            while (!done) {
+                selected = blocks.get(get);
+                selected.setOption(selected.getOption() + 1);
+                if (selected.getOption() == (attacks.size() - 1)) {
+                    if (selected != blocks.get(blocks.size() - 1)) {
+                        selected.setOption(0);
+                        get += 1;
+                    } else {
+                        done = true;
+                    }
+                } else {
+                    done = true;
+                }
+            }
+        }
+        assert(check);
+        assert (loops == 100000);
+    }
+
+    public void move() {
+
+    }
+
 
     @Test
     public void testAddStrategy() {
